dfa3c7b8be3f1a9d13b7b0f1d021166e652b91e9
AnhMinh Le <minhbac333studyus@gmail.com>
2021-04-26 16:29:43 -0500

    create menu in the main function

--- DiskSchedulingProject/DiskScheduling.h
+++ DiskSchedulingProject/DiskScheduling.h
@@ -10,8 +10,9 @@ private:
     int m_header = 50;
     int m_previous_request = 40 ;
     SchedulingAlgorithm* m_strategy{};
-    DiskScheduling* disk;
+    DiskScheduling* disk{};
 public:
+    DiskScheduling() : m_header(50), m_previous_request(40) {};
     const vector<int> getCyliaders();
     const int getHeader();
     const int getPreviousRequest();
--- DiskSchedulingProject/DiskSchedulingProject.cpp
+++ DiskSchedulingProject/DiskSchedulingProject.cpp
@@ -3,21 +3,65 @@
 #include "DiskScheduling.h"
 #include "SchedulingAlgorithm.h"
 #include "Fcfs_algorithm.h"
+#include <ctime>
+#include<random>
 using namespace std;
- 
+int produceRandom();
+void initualizeData(DiskScheduling& ds);
+void showMenu();
 int main()
 {
-    std::cout << "Hello World!\n";
+    char userOption;
     DiskScheduling ds;
-    for (int i = 0; i < 10; i++) {
-        ds.addValue(i);
-    }
-   
+    initualizeData(ds);
     Fcfs_algorithm fcfs;
+
+
+    cin >> userOption;
+    do {
+        showMenu(); 
+        cin >> userOption;
+        if (userOption == '1') {
+            ds.setStrategy(&fcfs); 
+            ds.run(&ds);
+        }
+      
+    }   
+    while (userOption == 'Q' || userOption == 'q');
+
+
+  
    
-    
-    ds.setStrategy(&fcfs);
-    
-    ds.run(&ds);
+    return 0;
 }
+
  
+int produceRandom() {  
+    random_device rd; 
+    // Initialize Mersenne Twister pseudo-random number generator
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1, 200);
+    int a = dis(gen);
+    return a;
+ 
+ }
+void initualizeData(DiskScheduling& ds) {
+    cout << "\nGenerating program....";
+
+    for (unsigned int i = 0; i < 10; i++) {
+        ds.addValue(produceRandom());
+    }
+    cout << "\nGenerating program successfully...\n\n";
+}
+void showMenu(){
+    cout << "\n--------------------------------DISK SCHEDULE ALGORITHM-----------------------------";
+    cout << "\n|\tType your choice to run the algorithm                                       |";
+    cout << "\n|\t1.First come first serve algorithm                                          |";
+    cout << "\n|\t2.Shortest seeking time algorithm                                           |";
+    cout << "\n|\t3.Scan algorithm                                                            |";
+    cout << "\n|\t4.C-Scan algorithm                                                          |";
+    cout << "\n|\t5.Look algorithm                                                            |";
+    cout << "\n|\t6.C-Look algorithm                                                          |";
+    cout << "\n|\tType Q to quit program                                                      |";
+    cout << "\nType your option...: ";
+}
